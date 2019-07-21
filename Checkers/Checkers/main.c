ddf0f15e97ae357e1ca0f405f906a53004283a33
Almog16 <aeshimon@gmail.com>
2019-07-21 23:09:53 +0300

    Added resetBoard function, which creates a default board. Moreover, added StoreBoard function, which writes a given board, in its current position, to a binary file (need to add the actual writing, but the logic is there).

--- Checkers/Checkers/main.c
+++ Checkers/Checkers/main.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 
 #define BOARD_SIZE 8
+#define Tmask 00000001
+#define Bmask 00000010
 
 typedef struct _checkersPos {
 	char row, col;
@@ -60,7 +62,37 @@ void Turn(Board board, Player player) {
 
 //Q5:
 void StoreBoard(Board board, char *filename) {
-
+	unsigned char curChar;
+	int i, j;
+	for (i = 0; i < BOARD_SIZE; i++) {
+		curChar = 0;
+		if (i % 2 == 0) {
+			for (j = 1; j < BOARD_SIZE; j += 2) {
+				curChar << 4;
+				if (board[i][j] == 'T')
+					curChar |= Tmask;
+				else if (board[i][j] == 'B')
+					curChar |= Bmask;
+				if (j == 3 || j == 7) {
+					//TODO: write curChar to file
+					curChar = 0;
+				}
+			}
+		}
+		else {
+			for (j = 0; j < BOARD_SIZE; j += 2) {
+				curChar << 4;
+				if (board[i][j] == 'T')
+					curChar |= Tmask;
+				else if (board[i][j] == 'B')
+					curChar |= Bmask;
+				if (j == 2 || j == 6) {
+					//TODO: write curChar to file
+					curChar = 0;
+				}
+			}
+		}
+	}
 }
 
 //Q6:
@@ -71,4 +103,31 @@ voidLoadBoard(char *filename, Board board) {
 //Q7:
 void PlayGame(Board board, Player starting_player) {
 
+}
+
+void resetBoard(Board board) {
+	int i, j;
+	for (i = 0; i < BOARD_SIZE; i++) {
+		for (j = 0; j < BOARD_SIZE; j++) {
+			if (i == 3 || i == 4) //these lines are empty
+				board[i][j] = ' ';
+			else if (i % 2 == 0) {
+				if (j % 2 == 0)
+					board[i][j] = ' ';
+				else if (i == 0 || i == 2)
+					board[i][j] = 'T';
+				else
+					board[i][j] = 'B';
+			}
+			else {
+				if (j % 2 != 0)
+					board[i][j] = ' ';
+				else if (i == 1)
+					board[i][j] = 'T';
+				else
+					board[i][j] = 'B';
+			}
+		}
+	
+	}
 }
